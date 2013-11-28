1ce71f371ec24f9523f0d0478ca4f46020873f63
San Gillis <gillis.san@gmail.com>
2013-11-28 21:44:47 +0100

    socket factory finally working

--- js/connectFour.js
+++ js/connectFour.js
@@ -1,9 +1,30 @@
 'use strict';
 
-var connectFour = angular.module('connectFour', ['socket-io']);
+var connectFour = angular.module('connectFour', []);
 
-connectFour.controller('GameCtrl', function GameCtrl($scope){
-    $scope.game = new Game($scope);
+connectFour.factory('socket', function($rootScope) {
+    var socket = io.connect();
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                if(callback) {
+                    callback.apply(socket, args);
+                }
+            });
+        }
+    };
+});
+
+connectFour.controller('GameCtrl', function GameCtrl($scope, socket){
+    $scope.game = new Game();
 
     $scope.makeMove = function(game, x, y){
         var spot = availableSpot(x, y, game.pieces);
@@ -32,7 +53,7 @@ connectFour.controller('GameCtrl', function GameCtrl($scope){
     }
 });
 
-function Game($scope){
+function Game(){
     this.range = [0, 1, 2, 3, 4, 5, 6];
     this.pieces = [[], []];
     this.active_player = 0;
