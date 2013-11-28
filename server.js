f0824652f5f4cd7e01d5499046033e07c23d56d2
San Gillis <gillis.san@gmail.com>
2013-11-28 21:40:20 +0100

    serve socket.io.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
     res.sendfile('index.html')
@@ -10,6 +13,6 @@ app.get(/^(.+)$/, function(req, res){
     res.sendfile(__dirname + req.params[0]);
 });
 
-app.listen(3000, function(){
+server.listen(3000, function(){
     console.log('Listening on port 3000');
 });
