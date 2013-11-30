32cad29928672855dc97390634b9a3a545a3f676
San Gillis <gillis.san@gmail.com>
2013-11-30 11:11:04 +0100

    reorder make move logic to extra function

--- js/connectFour.js
+++ js/connectFour.js
@@ -5,18 +5,24 @@ var connectFour = angular.module('connectFour', ['socket-io']);
 connectFour.controller('GameCtrl', function GameCtrl($scope, socket){
     $scope.game = new Game();
 
-    $scope.makeMove = function(game, x, y){
+    // This function is called to submit a move, and decides if it was a valid
+    // move
+    $scope.move = function(game, x, y){
         var spot = availableSpot(x, y, game.pieces);
-        socket.emit('move', {move: 'move_made'}, function(){});
         if(spot[1] > -1){
-            game.pieces[game.active_player].push(spot);
-            var won = fourConnected(game.pieces[game.active_player]);
-            if(won){
-                game.gameMessage = "P" + (game.active_player + 1) + " won!"
-            } else {
-                game.active_player = (game.active_player + 1) % 2;
-                game.gameMessage = "P" + (game.active_player + 1) + " to move"
-            }
+            $scope.makeMove(game, spot);
+        }
+    }
+
+    // This function is called when a valid move was submitted
+    $scope.makeMove = function(game, spot){
+        game.pieces[game.active_player].push(spot);
+        var won = fourConnected(game.pieces[game.active_player]);
+        if(won){
+            game.gameMessage = "P" + (game.active_player + 1) + " won!"
+        } else {
+            game.active_player = (game.active_player + 1) % 2;
+            game.gameMessage = "P" + (game.active_player + 1) + " to move"
         }
     }
 
