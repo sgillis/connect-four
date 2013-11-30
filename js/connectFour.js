87ad6ddaa92219594e86bf7238e237a33190d39d
San Gillis <gillis.san@gmail.com>
2013-11-30 11:06:04 +0100

    sockets to separate module

--- js/connectFour.js
+++ js/connectFour.js
@@ -1,27 +1,6 @@
 'use strict';
 
-var connectFour = angular.module('connectFour', []);
-
-connectFour.factory('socket', function($rootScope) {
-    var socket = io.connect('http://localhost:3000');
-    return {
-        on: function(eventName, callback) {
-            socket.on(eventName, function() {
-                var args = arguments;
-                $rootScope.$apply(function() {
-                    callback.apply(socket, args);
-                });
-            });
-        },
-        emit: function(eventName, data, callback) {
-            socket.emit(eventName, data, function() {
-                if(callback) {
-                    callback.apply(socket, args);
-                }
-            });
-        }
-    };
-});
+var connectFour = angular.module('connectFour', ['socket-io']);
 
 connectFour.controller('GameCtrl', function GameCtrl($scope, socket){
     $scope.game = new Game();
